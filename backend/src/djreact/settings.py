01cfa08047c4ca59b8c5398ce477943d76c567a4
Evan Byrnes <evan.byrnes@ucdconnect.ie>
2020-12-14 17:38:32 +0000

    allowed hosts change3

--- backend/src/djreact/settings.py
+++ backend/src/djreact/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.49.238.63', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['52.49.238.63', 'localhost', '127.0.0.1',
+                 'ec2-52-49-238-63.eu-west-1.compute.amazonaws.com']
 
 
 # Application definition
