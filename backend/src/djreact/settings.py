33050ad414de6722f16fc774e8cdc115eef624cd
Evan Byrnes <evan.byrnes@ucdconnect.ie>
2020-12-14 17:20:03 +0000

    allowed hosts

--- backend/src/djreact/settings.py
+++ backend/src/djreact/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'www.ec2-52-49-238-63.eu-west-1.compute.amazonaws.com'
+]
 
 
 # Application definition
